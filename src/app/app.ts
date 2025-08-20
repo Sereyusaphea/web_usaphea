1d9f4e8091c9f2297d8bb70f7156ed58d7132bd2
Sereyusaphea <sereyusaphea68@gmail.com>
2025-08-21 03:17:02 +0700

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from './navbar/navbar';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('inputdata');
+
 }
--- src/app/models/products.model.ts
+++ src/app/models/products.model.ts
@@ -1,14 +1,17 @@
-interface Rating {
-  rate: number;
-  count: number;
-}
-
 export interface Product {
   id: number;
   title: string;
   price: number;
   description: string;
-  image: string;
   category: string;
-  rating: Rating;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
 }
